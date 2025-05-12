04506302bdda26d5a6c5cd0fff9edc31c49c5bca
omnya zerghouni <omnyazer@gmail.com>
2025-05-12 13:35:23 +0200

    activite4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path=":category" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,6 @@
+const NotFound = () => {
+    return <h1>Page non trouvée</h1>;
+  };
+  
+  export default NotFound;
+  
