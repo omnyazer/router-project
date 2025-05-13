49989729cc7e0753e564bf7c7fbd5b9e94b4efa6
omnya zerghouni <omnyazer@gmail.com>
2025-05-13 11:30:00 +0200

    activite 8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,21 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 const Category = () => {
   const { categoryId } = useParams();
+  const navigate = useNavigate();
 
-  return <h1>Catégorie : {categoryId}</h1>;
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="text-center">
+      <h1>Catégorie : {categoryId}</h1>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Retour à la page d'accueil
+      </button>
+    </div>
+  );
 };
 
 export default Category;
