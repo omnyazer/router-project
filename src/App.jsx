4411bd3da8d24abf9b5d3beb9de1956653cff653
omnya zerghouni <omnyazer@gmail.com>
2025-05-13 14:59:58 +0200

    activite 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
+import Register from "./pages/Register"; 
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="category/:categoryId" element={<Category />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   );
